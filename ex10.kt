8e9de56d12123ee485c7f276e3d7e9c3a3dbb8af
Murilo0123 <98995528+Murilo0123@users.noreply.github.com>
2026-08-13 02:49:25 -0300

    Adicionei as saídas

--- ex10.kt
+++ ex10.kt
@@ -1,17 +1,19 @@
-// 1) Função completa recebendo o comportamento (onClick) como parâmetro
 fun botao(rotulo: String, onClick: () -> Unit) {
     println("[$rotulo] clicado")
-    onClick() // Executa o código recebido
+    onClick()
 }
 
 fun main() {
-    // 2) Chamada usando a sintaxe de trailing lambda
-    // A lambda sai dos parênteses porque é o último parâmetro
+    
     botao("Enviar") {
         println("Ação executada: dados enviados com sucesso!")
+        //[Enviar] clicado
+        //Ação executada: dados enviados com sucesso!
     }
     
     botao("Cancelar") {
         println("Ação executada: operação cancelada.")
+        //[Cancelar] clicado
+        //Ação executada: operação cancelada.
     }
 }   
