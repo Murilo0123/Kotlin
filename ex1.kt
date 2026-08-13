45da3dd7c31f1fdc052bbde8c0036028fb4b81b9
Murilo Monteiro <murilomsilva06@gmail.com>
2026-08-13 00:45:44 -0300

    Commmit

---
+++ ex1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val disciplina = "Programação Mobile"
+    var aulas = 20
+    aulas = 21
+    println("$disciplina tem $aulas aulas")
+}
+
+// alterei "val aulas" para "var aulas", pois diferente de val, var declara como variavel mutável.
